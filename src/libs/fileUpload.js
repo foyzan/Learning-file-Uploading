455a954cde9b40c5c9016e4a213f2e37d505679e
foyzan <foyzanahmedapon01@gmail.com>
2026-03-26 17:33:25 +0600

    added file storage and file name replace for security

--- src/libs/fileUpload.js
+++ src/libs/fileUpload.js
@@ -1,5 +1,24 @@
 const multer = require('multer')
+const crypto = require('crypto')
 const path = require('path')
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, done)=>{
+        done(null, path.join(process.cwd(), 'upload'),)
+    },
+    filename: (req, file, done)=>{
+
+        // get file extension
+        const fileExt = path.extname(file.originalname).toLowerCase()
+
+        // get random name string
+        const randomName = crypto.randomBytes(16).toString('hex');
+
+        done(null, `${randomName}${fileExt}`)
+    }
+})
+
 
 const fileFilter = (req, file, done)=>{
     const allowedMimeTypes = ['image/jpeg', 'image/png'];
@@ -14,6 +33,7 @@ const fileFilter = (req, file, done)=>{
 
 
 const upload = multer({
+    storage: storage,
     dest: path.join(process.cwd(), 'upload'),
     fileFilter: fileFilter,
     limits: {
